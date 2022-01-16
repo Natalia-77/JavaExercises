0b4d8c67653504e855520dfbb34be34f723cfe58
vlasnata80@gmail.com <vlasnata80@gmail.com>
2022-01-16 21:01:27 +0200

    Add task9-10

---
+++ src/main/java/task10/Main.java
@@ -0,0 +1,62 @@
+package task10;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Random random = new Random();
+        List<Integer> positive= new ArrayList<>();
+        List<Integer> negative= new ArrayList<>();
+        List<Integer> multiply= new ArrayList<>();
+        List<Integer> nonMultiply= new ArrayList<>();
+        int[] arr = new int[20];
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = 100 - random.nextInt(200);
+        for (int item : arr) {
+            System.out.print(item + " ");
+        }
+        System.out.println();
+
+        for(int j=0;j<arr.length;j++){
+            if(arr[j]>0){
+
+                positive.add(arr[j]);
+            }
+            if(arr[j]<0){
+                negative.add(arr[j]);
+            }
+            if(arr[j]%2==0){
+                multiply.add(arr[j]);
+            }
+            if(arr[j]%2!=0){
+                nonMultiply.add(arr[j]);
+            }
+        }
+        System.out.println("Positive elements list:");
+        for (Integer item:positive) {
+            System.out.print(item+" ");
+        }
+        System.out.println();
+        System.out.println("Negative elements list:");
+        for (Integer item:negative) {
+            System.out.print(item+" ");
+
+        }
+        System.out.println();
+        System.out.println("Multiply elements list:");
+        for (Integer item:multiply) {
+            System.out.print(item+" ");
+
+        }
+        System.out.println();
+        System.out.println("Non-Multiply elements list:");
+        for (Integer item:nonMultiply) {
+            System.out.print(item+" ");
+
+        }
+    }
+}
--- src/main/java/task9/Main.java
+++ src/main/java/task9/Main.java
@@ -1,7 +1,47 @@
 package task9;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Random random = new Random();
+        int n = 30, positive = 0, negative = 0, zero = 0;
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = 100 - random.nextInt(200);
+        for (int item : arr) {
+            System.out.print(item + " ");
+        }
+        System.out.println();
 
+        int min = arr[0], max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 0) {
+                negative++;
+            }
+            if (arr[i] > 0) {
+                positive++;
+            }
+            if (arr[i] == 0) {
+                zero++;
+            }
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println("Minimal:  " + min);
+        System.out.println("Max:  " + max);
+        System.out.println("Positive:  " + positive);
+        System.out.println("Negative:  " + negative);
+        if (zero > 0)
+            System.out.println("Zero:  " + zero);
+        else {
+            System.out.println("There isn't zero elements");
+        }
     }
 }
